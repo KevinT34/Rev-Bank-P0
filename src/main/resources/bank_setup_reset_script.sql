e44a8e686f2912333a0db2d60994f4362d74c32e
Kevin Toyloy <kevintoyloy34@gmail.com>
2024-07-10 11:18:07 -0400

    Uploading sql files

---
+++ src/main/resources/bank_setup_reset_script.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS "user";
+
+
+
+CREATE TABLE "user" (
+	username TEXT,
+	password TEXT
+);
+
+INSERT INTO "user" VALUES ('admin', 1234);
+INSERT INTO "user" VALUES ('admin2', 5678);
+
+
+
